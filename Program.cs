553965c04a2ad6a997881597ff0ec8f7c774c068
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-05 22:16:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ class Program
     {
         var tiStation = new TiStation(); 
 
-        tiStation.Addcar(new car("A111AA", DateTime.Now.AddDays(3), "Автомат")); 
-        tiStation.Addcar(new car("B222BB", DateTime.Now.AddDays(1), "Механика"));
+        tiStation.AddCar(new Car("A111AA", DateTime.Now.AddDays(3), "Автомат")); 
+        tiStation.AddCar(new Car("B222BB", DateTime.Now.AddDays(1), "Механика"));
         tiStation.AddMotorcycle(new Motorcycle("C333CC", DateTime.Now.AddDays(2), "Спортивный"));
         tiStation.AddMotorcycle(new Motorcycle("D444DD", DateTime.Now.AddDays(5), "Классика"));
         tiStation.AddMotorcycle(new Motorcycle("E555EE", DateTime.Now.AddDays(4), "Чоппер"));
 
-        foreach (var ti in tiStation.scheduleе())
+        foreach (var vehicle in tiStation.Schedule())
         {
-            Console.WriteLine(((dynamic)ti).GovNumber + " - " + ((dynamic)ti).TiData.ToString("dd.MM.yyyy")); 
+            Console.WriteLine(vehicle.GovNumber + " - " + vehicle.TiData.ToString("dd.MM.yyyy")); 
         }
         Console.ReadKey();
     }
