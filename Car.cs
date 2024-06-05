26bb05636c4fe593afe908a3ea58e9cc1abdb514
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-05 22:18:22 +0300

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -2,18 +2,14 @@ using System;
 
 namespace STO 
 {
-    public class car 
-        
+    public class Car : Vehicle
     {
-        public string GovNumber { get; set; } 
-        public DateTime TiData { get; set; } 
-        public string TransmissionType { get; set; } 
+        public string TransmissionType { get; set; }
 
-        public car(string govNumber, DateTime tiData, string transmissionType) 
+        public Car(string govNumber, DateTime tiData, string transmissionType)
         {
             GovNumber = govNumber;
             TiData = tiData;
             TransmissionType = transmissionType;
         }
     }
-}
