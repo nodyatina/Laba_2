057e5e8ad72106e695680c6be3994d1eaa7f95c4
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-05 22:20:30 +0300

    Update Motorcycle.cs

--- Motorcycle.cs
+++ Motorcycle.cs
@@ -2,17 +2,14 @@ using System;
 
 namespace STO
 {
-    public class Motorcycle
+   public class Motorcycle : Vehicle
     {
-        public string GovNumber { get; set; }
-        public DateTime TiData { get; set; }
-        public string MotoType { get; set; } 
+        public string MotoType { get; set; }
 
-        public Motorcycle(string govNumber, DateTime tiData, string motoType) 
+        public Motorcycle(string govNumber, DateTime tiData, string motoType)
         {
             GovNumber = govNumber;
             TiData = tiData;
             MotoType = motoType;
         }
     }
-}
