f9a49e24b5ccb9a71a22dbc1d16e78997f971d3b
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-05 22:21:15 +0300

    Update TiStation.cs

--- TiStation.cs
+++ TiStation.cs
@@ -6,46 +6,21 @@ namespace STO
 {
     public class TiStation
     {
-        private List<Car> cars;
-        private List<Motorcycle> motorcycles;
-
-        public TiStation()
-        {
-            cars = new List<Car>();
-            motorcycles = new List<Motorcycle>();
-        }
+        private List<Vehicle> vehicles = new List<Vehicle>();
 
         public void AddCar(Car car)
         {
-            if (car != null)
-            {
-                cars.Add(car);
-            }
-            else
-            {
-                throw new ArgumentNullException("car", "Cannot add a null car to the station.");
-            }
+            vehicles.Add(car);
         }
 
         public void AddMotorcycle(Motorcycle motorcycle)
         {
-            if (motorcycle != null)
-            {
-                motorcycles.Add(motorcycle);
-            }
-            else
-            {
-                throw new ArgumentNullException("motorcycle", "Cannot add a null motorcycle to the station.");
-            }
+            vehicles.Add(motorcycle);
         }
 
-        public IEnumerable<Vehicle> Schedule()
+        public List<Vehicle> Schedule()
         {
-            List<Vehicle> schedule = new List<Vehicle>();
-            schedule.AddRange(cars);
-            schedule.AddRange(motorcycles);
-            schedule.Sort((x, y) => x.TiData.CompareTo(y.TiData));
-            return schedule;
+            return vehicles;
         }
     }
 }
