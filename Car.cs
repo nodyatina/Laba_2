b9970dd69bcfb2615818790d02b42f5f72b645e2
nodyatina <167766275+nodyatina@users.noreply.github.com>
2024-06-08 00:08:30 +0300

    Update Car.cs

--- Car.cs
+++ Car.cs
@@ -8,7 +8,7 @@ namespace STO
 
         public Car(string govNumber, DateTime tiData, string transmissionType)
         {            
-             if (string.IsNullOrEmpty(govNumber)) throw new ArgumentNullException(nameof(govNumber), "GovNumber cannot be null or empty.");
+             if (string.IsNullOrEmpty(govNumber)) throw new ArgumentNullException("GovNumber cannot be null or empty.");
             GovNumber = govNumber;
             TiData = tiData;
             TransmissionType = transmissionType;
